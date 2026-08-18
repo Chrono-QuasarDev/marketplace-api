91b8f15d6606f87e992d74c0a78457227e14566d
QuasarDev <quasardev9@gmail.com>
2026-08-18 16:12:42 +0000

    feat(product): make getProduct and getProductById route public

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import app from './src/app.js'
 const testConnection = async () => {
   try {
     await sequelize.authenticate();
-    app.listen(3000, () => { console.log('Server is running on port 3000'); });
+    app.listen(3000, () => { console.log('Server is at http://localhost:3000/api/products'); });
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
--- src/modules/products/product.route.js
+++ src/modules/products/product.route.js
@@ -1,14 +1,22 @@
 import { Router } from "express";
 import { authenticate } from '../../shared/middleware/auth.middleware.js';
 import { authorization } from '../../shared/middleware/authz.middleware.js';
-import { createProduct, getProducts, getProductById, updateProductById, deleteProductById } from './product.controller.js';
+import { 
+  createProduct, 
+  getProducts, 
+  getProductById, 
+  updateProductById, 
+  deleteProductById 
+} from './product.controller.js';
 
 const router = Router();
-router.use(authenticate);
 
-router.post('/', authorization(['seller']), createProduct);
-router.get('/', authorization(['buyer', 'seller', 'admin']), getProducts);
+// Public 
+router.get('/', getProducts);
 router.get('/:id', getProductById);
+
+// Protected
+router.post('/', authorization(['seller']), createProduct);
 router.put('/:id', authorization(['seller']), updateProductById);
 router.delete('/:id', authorization(['seller']), deleteProductById);
 
